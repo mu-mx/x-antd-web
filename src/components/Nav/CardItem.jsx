e5c25f2949c706cc31a3fd6ffa36cdf07b2b5f2d
mu-mx <1017182993@qq.com>
2024-06-07 14:04:06 +0800

    改版

--- src/components/Nav/CardItem.jsx
+++ src/components/Nav/CardItem.jsx
@@ -1,256 +1,257 @@
 // @ts-nocheck
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from "react";
+import React from 'react';
 
 import {
-    Button,
-    Col,
-    Row,
-    Input,
-    Select,
-    Card,
-    Tabs,
-    Collapse,
-    theme,
-    Avatar,
-    Radio,
-    Tooltip,
-    message,
-    List,
-} from "antd";
+  Button,
+  Col,
+  Row,
+  Input,
+  Select,
+  Card,
+  Tabs,
+  Collapse,
+  theme,
+  Avatar,
+  Radio,
+  Tooltip,
+  message,
+  List,
+} from 'antd';
 
 import Icon, {
-    SearchOutlined,
-    CaretRightOutlined,
-    CopyOutlined,
-    EditOutlined,
-    ForwardOutlined,
-} from "@ant-design/icons";
+  SearchOutlined,
+  CaretRightOutlined,
+  CopyOutlined,
+  EditOutlined,
+  ForwardOutlined,
+  CloudOutlined,
+} from '@ant-design/icons';
 
-import copy from "copy-to-clipboard";
+import copy from 'copy-to-clipboard';
 
 const ClickableCardBack = ({ icon, title, description, url }) => {
-    return (
-        <>
-            <Tooltip
-                placement="top"
-                title={description}
-            >
-                <a
-                    href={url}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <Card
-                        hoverable
-                        bodyStyle={{
-                            padding: "12px 8px",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        {icon ? (
-                            <Avatar
-                                style={{
-                                    verticalAlign: "middle",
-                                }}
-                                size="large"
-                                src={icon}
-                            />
-                        ) : (
-                            <Avatar
-                                style={{
-                                    verticalAlign: "middle",
-                                }}
-                                size="large"
-                            >
-                                X
-                            </Avatar>
-                        )}
-                        <div className="ml-2 truncate flex-1">
-                            <p className="truncate ">{title}</p>
-                            <p className="truncate ">{description}</p>
-                        </div>
-                    </Card>
-                </a>
-            </Tooltip>
-        </>
-    );
+  return (
+    <>
+      <Tooltip
+        placement='top'
+        title={description}
+      >
+        <a
+          href={url}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Card
+            hoverable
+            bodyStyle={{
+              padding: '12px 8px',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            {icon ? (
+              <Avatar
+                style={{
+                  verticalAlign: 'middle',
+                }}
+                size='large'
+                src={icon}
+              />
+            ) : (
+              <Avatar
+                style={{
+                  verticalAlign: 'middle',
+                }}
+                size='large'
+              >
+                X
+              </Avatar>
+            )}
+            <div className='ml-2 truncate flex-1'>
+              <p className='truncate '>{title}</p>
+              <p className='truncate '>{description}</p>
+            </div>
+          </Card>
+        </a>
+      </Tooltip>
+    </>
+  );
 };
 
 const DefaultIcon = (value) => (
-    <svg
-        width="22"
-        height="22"
-        viewBox="0 0 48 48"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path
-            d="M2 23.3548H11L17.8889 4L28.8889 44L37 23.3548H46"
-            stroke="#333"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
-    </svg>
+  <svg
+    width='22'
+    height='22'
+    viewBox='0 0 48 48'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M2 23.3548H11L17.8889 4L28.8889 44L37 23.3548H46'
+      stroke='#333'
+      strokeWidth='4'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
 );
 
 const ClickableCard = ({ icon, title, description, url }) => {
-    const [messageApi, contextHolder] = message.useMessage();
+  const [messageApi, contextHolder] = message.useMessage();
 
-    return (
-        <>
-            {contextHolder}
+  return (
+    <>
+      {contextHolder}
 
-            <Card
-                hoverable
-                size="small"
-                className="small-card"
-                loading={!url}
-                bodyStyle={{
-                    padding: "12px 8px",
-                    display: "flex",
-                    alignItems: "center",
-                }}
-                actions={[
-                    <Tooltip
-                        key="copy"
-                        placement="top"
-                        title={"复制 " + url}
-                    >
-                        <CopyOutlined onClick={() => copy(url)} />
-                    </Tooltip>,
+      <Card
+        hoverable
+        size='small'
+        className='small-card'
+        loading={!url}
+        bodyStyle={{
+          padding: '12px 8px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        actions={[
+          <Tooltip
+            key='copy'
+            placement='top'
+            title={'复制 ' + url}
+          >
+            <CopyOutlined onClick={() => copy(url)} />
+          </Tooltip>,
 
-                    <Tooltip
-                        placement="top"
-                        key="open"
-                        title={`打开 ${url}`}
-                    >
-                        <a
-                            href={url}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <ForwardOutlined />
-                        </a>
-                    </Tooltip>,
-                ]}
+          <Tooltip
+            placement='top'
+            key='open'
+            title={`打开 ${url}`}
+          >
+            <a
+              href={url}
+              target='_blank'
+              rel='noreferrer'
             >
-                <Tooltip
-                    placement="top"
-                    key="open"
-                    title={`打开 ${url}`}
-                >
-                    <a
-                        href={url}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <Avatar
-                            style={{
-                                verticalAlign: "middle",
-                            }}
-                            size="small"
-                            src={icon ? icon : <DefaultIcon />}
-                            alt="X"
-                        />
-                    </a>
-                </Tooltip>
+              <ForwardOutlined />
+            </a>
+          </Tooltip>,
+        ]}
+      >
+        <Tooltip
+          placement='top'
+          key='open'
+          title={`打开 ${url}`}
+        >
+          <a
+            href={url}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Avatar
+              style={{
+                verticalAlign: 'middle',
+              }}
+              size='small'
+              src={icon ? icon : <DefaultIcon />}
+              alt='X'
+            />
+          </a>
+        </Tooltip>
 
-                <Tooltip
-                    placement="bottom"
-                    title={description}
-                >
-                    <div
-                        className="ml-4 truncate   flex-1"
-                        onClick={() => {
-                            copy(title);
-                            messageApi.open({
-                                type: "success",
-                                content: `复制成功, ${title}`,
-                            });
-                        }}
-                    >
-                        <p className="truncate mb-0.5 font-bold text-base">{title}</p>
-                        <p className=" truncate  text-xs">{description}</p>
-                    </div>
-                </Tooltip>
-            </Card>
-        </>
-    );
+        <Tooltip
+          placement='bottom'
+          title={description}
+        >
+          <div
+            className='ml-4 truncate   flex-1'
+            onClick={() => {
+              copy(title);
+              messageApi.open({
+                type: 'success',
+                content: `复制成功, ${title}`,
+              });
+            }}
+          >
+            <p className='truncate mb-0.5 font-bold text-base'>{title}</p>
+            <p className=' truncate  text-xs'>{description}</p>
+          </div>
+        </Tooltip>
+      </Card>
+    </>
+  );
 };
 
 const getItems = (panelStyle, data) => {
-    return data.map((it, key) => ({
-        key,
-        label: <Tooltip title={it.description}>{it.title}</Tooltip>,
-        children: (
-            <>
-                <Row gutter={[10, 16]}>
-                    <Col
-                        xs={24}
-                        sm={24}
-                        md={12}
-                        lg={8}
-                        xl={6}
-                        xxl={4}
-                        key={key}
-                    >
-                        <ClickableCard {...it} />
-                    </Col>
-                </Row>
-            </>
-        ),
-        style: panelStyle,
-    }));
+  return data.map((it, key) => ({
+    key,
+    label: <Tooltip title={it.description}>{it.title}</Tooltip>,
+    children: (
+      <>
+        <Row gutter={[10, 16]}>
+          <Col
+            xs={24}
+            sm={24}
+            md={12}
+            lg={8}
+            xl={6}
+            xxl={4}
+            key={key}
+          >
+            <ClickableCard {...it} />
+          </Col>
+        </Row>
+      </>
+    ),
+    style: panelStyle,
+  }));
 };
 
 // eslint-disable-next-line react/prop-types
 function CardItem({ data }) {
-    const { token } = theme.useToken();
+  const { token } = theme.useToken();
 
-    const panelStyle = {
-        padding: 8,
-        marginBottom: 24,
-        background: token.colorFillAlter,
-        borderRadius: token.borderRadiusLG,
-        border: "none",
-    };
+  const panelStyle = {
+    padding: 8,
+    marginBottom: 24,
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: 'none',
+  };
 
-    const defaultAllKey = new Array(10).fill(0).map((it, ind) => ind);
+  const defaultAllKey = new Array(10).fill(0).map((it, ind) => ind);
 
-    const mouseEvent = new MouseEvent("click", {
-        //创建一个 click 的鼠标事件 并让他点击
-        bubbles: true,
-        cancelable: true,
-    });
+  const mouseEvent = new MouseEvent('click', {
+    //创建一个 click 的鼠标事件 并让他点击
+    bubbles: true,
+    cancelable: true,
+  });
 
-    const tabHover = () => {
-        var els = document.querySelectorAll(".two-tabs .ant-tabs-tab");
-        var top = document.querySelectorAll(".two-tabs .ant-tabs-tab-btn");
-        for (let i = 0; i < top.length; i++) {
-            top[i].addEventListener("mouseover", function () {
-                //给某个 dom 在=绑定 mouseover 事件
-                els[i].dispatchEvent(mouseEvent); // 在 mouseover 中将想要进行的 click 通过 dispatchEvent 事件派发给将要发生 click 的 div
-            });
-        }
-    };
+  const tabHover = () => {
+    var els = document.querySelectorAll('.two-tabs .ant-tabs-tab');
+    var top = document.querySelectorAll('.two-tabs .ant-tabs-tab-btn');
+    for (let i = 0; i < top.length; i++) {
+      top[i].addEventListener('mouseover', function () {
+        //给某个 dom 在=绑定 mouseover 事件
+        els[i].dispatchEvent(mouseEvent); // 在 mouseover 中将想要进行的 click 通过 dispatchEvent 事件派发给将要发生 click 的 div
+      });
+    }
+  };
 
-    React.useEffect(() => {
-        tabHover();
+  React.useEffect(() => {
+    tabHover();
 
-        // if (renderNextTime.current) {
-        //     renderNextTime.current = false;
-        // } else {
-        //     tabHover();
-        // }
-    });
+    // if (renderNextTime.current) {
+    //     renderNextTime.current = false;
+    // } else {
+    //     tabHover();
+    // }
+  });
 
-    return (
-        <>
-            {/* <Card
+  return (
+    <>
+      {/* <Card
                 title={data.title}
                 bordered
                 style={{
@@ -274,62 +275,62 @@ function CardItem({ data }) {
                 </Row>
             </Card> */}
 
-            <Card
-                title={data.title}
-                style={{
-                    marginTop: "16px",
-                }}
-            >
-                <List
-                    itemLayout="horizontal"
-                    bordered
-                    size="small"
-                    dataSource={data.childrens}
-                    renderItem={(item, index) => (
-                        <List.Item
-                            actions={[
-                                <Tooltip
-                                    key="copy"
-                                    placement="top"
-                                    title={"复制 " + item.url}
-                                >
-                                    <CopyOutlined onClick={() => copy(item.url)} />
-                                </Tooltip>,
+      <Card
+        title={data.title}
+        style={{
+          marginTop: '16px',
+        }}
+      >
+        <List
+          itemLayout='vertical'
+          bordered
+          split
+          size='middle'
+          dataSource={data.childrens}
+          renderItem={(item, index) => (
+            <List.Item
+              actions={[
+                <a
+                  key='copy'
+                  onClick={() => copy(item.url)}
+                >
+                  <CopyOutlined /> 复制链接
+                </a>,
 
-                                <Tooltip
-                                    placement="top"
-                                    key="open"
-                                    title={`打开 ${item.url}`}
-                                >
-                                    <a
-                                        href={item.url}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        <ForwardOutlined />
-                                    </a>
-                                </Tooltip>,
-                            ]}
-                        >
-                            <Tooltip
-                                placement="top"
-                                key="openItem"
-                                title={`${item.description}`}
-                            >
-                                <List.Item.Meta
-                                    avatar={
-                                        <Avatar src={item.icon ? item.icon : <DefaultIcon />} />
-                                    }
-                                    title={<a>{item.title}</a>}
-                                    description={<a>{item.description}</a>}
-                                />
-                            </Tooltip>
-                        </List.Item>
-                    )}
-                />
-            </Card>
-        </>
-    );
+                <a
+                  href={item.url}
+                  key='open'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <CloudOutlined /> 新标签打开
+                </a>,
+              ]}
+            >
+              <List.Item.Meta
+                avatar={
+                  <Avatar
+                    size={42}
+                    src={item.icon ? item.icon : <DefaultIcon />}
+                  />
+                }
+                title={<a>{item.title}</a>}
+                description={
+                  <>
+                    <a>{item.description}</a>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      </Card>
+    </>
+  );
 }
 
 export default CardItem;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,125 +1,125 @@
 // @ts-nocheck
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import {
-    Button,
-    Col,
-    Row,
-    Input,
-    Select,
-    Card,
-    Tabs,
-    Collapse,
-    theme,
-    Avatar,
-    Radio,
-    Tooltip,
-    FloatButton,
-    Anchor,
-} from "antd";
+  Button,
+  Col,
+  Row,
+  Input,
+  Select,
+  Card,
+  Tabs,
+  Collapse,
+  theme,
+  Avatar,
+  Radio,
+  Tooltip,
+  FloatButton,
+  Anchor,
+} from 'antd';
 
-import StickyBox from "react-sticky-box";
-import { RedoOutlined } from "@ant-design/icons";
+import StickyBox from 'react-sticky-box';
+import { RedoOutlined } from '@ant-design/icons';
 
-import CardItem from "./CardItem";
-import { getDataBase, fetchData } from "@/utils/data";
+import CardItem from './CardItem';
+import { getDataBase, fetchData } from '@/utils/data';
 
 export default function Index() {
-    const [mode, setMode] = React.useState("top");
+  const [mode, setMode] = React.useState('top');
 
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const renderNextTime = useRef(true);
+  const renderNextTime = useRef(true);
 
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-    const renderTabBar = (props, DefaultTabBar) => (
-        <StickyBox
-            offsetTop={0}
-            offsetBottom={20}
-            style={{ zIndex: 1 }}
+  const renderTabBar = (props, DefaultTabBar) => (
+    <StickyBox
+      offsetTop={0}
+      offsetBottom={20}
+      style={{ zIndex: 1 }}
+    >
+      <DefaultTabBar
+        {...props}
+        style={{ background: colorBgContainer }}
+      />
+    </StickyBox>
+  );
+
+  const getList = async () => {
+    const data = await getDataBase();
+    console.log('data - >:', data);
+    setData(data);
+  };
+
+  const mouseEvent = new MouseEvent('click', {
+    //创建一个 click 的鼠标事件 并让他点击
+    bubbles: true,
+    cancelable: true,
+  });
+
+  const tabHover = () => {
+    var els = document.querySelectorAll('.ant-tabs-tab');
+    var top = document.querySelectorAll('.ant-tabs-tab-btn');
+    for (let i = 0; i < top.length; i++) {
+      top[i].addEventListener('mouseover', function () {
+        //给某个 dom 在=绑定 mouseover 事件
+        els[i].dispatchEvent(mouseEvent); // 在 mouseover 中将想要进行的 click 通过 dispatchEvent 事件派发给将要发生 click 的 div
+      });
+    }
+  };
+
+  // useEffect(() => {
+  //     if (renderNextTime.current) {
+  //         renderNextTime.current = false;
+  //     } else {
+  //         tabHover();
+  //     }
+  // });
+
+  useEffect(() => {
+    getList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Row>
+        <Col
+          span={4}
+          offset={2}
         >
-            <DefaultTabBar
-                {...props}
-                style={{ background: colorBgContainer }}
-            />
-        </StickyBox>
-    );
+          <Anchor
+            affix={false}
+            style={{ maxHeight: '60vh', overflowY: 'auto' }}
+            targetOffset={32}
+            getContainer={() => document.querySelector('#bigWrap')}
+            items={data.map((item, index) => ({
+              key: index + 1,
+              href: '#nav' + index,
+              title: item.title,
+            }))}
+          />
+        </Col>
 
-    const getList = async () => {
-        const data = await getDataBase();
-        console.log("data - >:", data);
-        setData(data);
-    };
+        <Col
+          span={14}
+          id='bigWrap'
+          style={{ overflowY: 'auto', height: '860px' }}
+        >
+          {data.map((_, i) => (
+            <div
+              id={'nav' + i}
+              key={i}
+              style={{ minHeight: '30vh' }}
+            >
+              <CardItem data={_} />
+            </div>
+          ))}
 
-    const mouseEvent = new MouseEvent("click", {
-        //创建一个 click 的鼠标事件 并让他点击
-        bubbles: true,
-        cancelable: true,
-    });
-
-    const tabHover = () => {
-        var els = document.querySelectorAll(".ant-tabs-tab");
-        var top = document.querySelectorAll(".ant-tabs-tab-btn");
-        for (let i = 0; i < top.length; i++) {
-            top[i].addEventListener("mouseover", function () {
-                //给某个 dom 在=绑定 mouseover 事件
-                els[i].dispatchEvent(mouseEvent); // 在 mouseover 中将想要进行的 click 通过 dispatchEvent 事件派发给将要发生 click 的 div
-            });
-        }
-    };
-
-    // useEffect(() => {
-    //     if (renderNextTime.current) {
-    //         renderNextTime.current = false;
-    //     } else {
-    //         tabHover();
-    //     }
-    // });
-
-    useEffect(() => {
-        getList();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-        <>
-            <Row>
-                <Col
-                    span={4}
-                    offset={2}
-                >
-                    <Anchor
-                        affix={false}
-                        style={{ maxHeight: "60vh", overflowY: "auto" }}
-                        targetOffset={32}
-                        getContainer={() => document.querySelector("#bigWrap")}
-                        items={data.map((item, index) => ({
-                            key: index + 1,
-                            href: "#nav" + index,
-                            title: item.title,
-                        }))}
-                    />
-                </Col>
-
-                <Col
-                    span={14}
-                    id="bigWrap"
-                    style={{ overflowY: "auto", height: "860px" }}
-                >
-                    {data.map((_, i) => (
-                        <div
-                            id={"nav" + i}
-                            key={i}
-                            style={{ minHeight: "30vh" }}
-                        >
-                            <CardItem data={_} />
-                        </div>
-                    ))}
-
-                    {/* <Card
+          {/* <Card
                         id="bigWrap"
                         title=""
                         className="daohang-body h-full"
@@ -144,35 +144,35 @@ export default function Index() {
                             </div>
                         ))}
                     </Card> */}
-                </Col>
-            </Row>
+        </Col>
+      </Row>
 
-            <FloatButton.BackTop
-                style={{
-                    left: "16%",
-                    bottom: "20%",
-                }}
-                target={() => document.querySelector("#bigWrap")}
-            />
+      <FloatButton.BackTop
+        style={{
+          left: '16%',
+          bottom: '20%',
+        }}
+        target={() => document.querySelector('#bigWrap')}
+      />
 
-            <FloatButton
-                style={{
-                    left: "16%",
-                    bottom: "14%",
-                }}
-                icon={
-                    <Tooltip title="刷新数据">
-                        <RedoOutlined />
-                    </Tooltip>
-                }
-                onClick={async () => {
-                    localStorage.clear();
-                    
-                    fetchData().then(() => {
-                        window.location.reload();
-                    });
-                }}
-            />
-        </>
-    );
+      <FloatButton
+        style={{
+          left: '16%',
+          bottom: '14%',
+        }}
+        icon={
+          <Tooltip title='刷新数据'>
+            <RedoOutlined />
+          </Tooltip>
+        }
+        onClick={async () => {
+          localStorage.clear();
+
+          fetchData().then(() => {
+            window.location.reload();
+          });
+        }}
+      />
+    </>
+  );
 }
