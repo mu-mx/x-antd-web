31004f8337b65f3a646f0abc5b4b85563b4738ab
mu-mx <1017182993@qq.com>
2024-06-23 14:40:57 +0800

    更换数据源

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,90 +1,96 @@
-export const get = (url) =>
-    fetch(url)
-        .then((response) => {
-            // network failure, request prevented
-            if (response.status >= 200 && response.status < 300) {
-                return Promise.resolve(response);
-            }
+export const get = (url, data) =>
+  fetch(url, data)
+    .then((response) => {
+      // network failure, request prevented
+      if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response);
+      }
 
-            return Promise.reject(new Error(response.statusText));
-        })
-        .then((response) => response.json())
-        .then((result) => result)
-        .catch((error) => {
-            console.log("error - >:", error);
-            return [];
-        });
+      return Promise.reject(new Error(response.statusText));
+    })
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => {
+      console.log('error - >:', error);
+      return [];
+    });
 
 const mapToTree = (data) => {
-    const res = [];
-    const map = {};
-    data.forEach((item) => {
-        map[item.id] = item;
-    });
-    data.forEach((item) => {
-        const parent = map[item.pId];
-        if (parent) {
-            (parent.children || (parent.children = [])).push(item);
-        } else {
-            res.push(item);
-        }
-    });
-    return res;
+  const res = [];
+  const map = {};
+  data.forEach((item) => {
+    map[item.id] = item;
+  });
+  data.forEach((item) => {
+    const parent = map[item.pId];
+    if (parent) {
+      (parent.children || (parent.children = [])).push(item);
+    } else {
+      res.push(item);
+    }
+  });
+  return res;
 };
 
 export const fetchData = async () => {
-    try {
-        const basePath = `https://raw.githubusercontent.com/mu-mx/site-database/main`;
+  try {
+    const categoryPath =
+      'https://ap-southeast-1.aws.data.mongodb-api.com/app/app-site-wdecree/endpoint/getCateAll';
 
-        const categoryPath = `${basePath}/category.json`;
-        const websitePath = `${basePath}/website.json`;
+    const websitePathReq =
+      'https://ap-southeast-1.aws.data.mongodb-api.com/app/app-site-wdecree/endpoint/getSiteAll';
 
-        await Promise.all([get(categoryPath), get(websitePath)]).then(
-            ([categoryData, websiteData]) => {
-                localStorage.setItem("cate", JSON.stringify(categoryData));
-                localStorage.setItem("site", JSON.stringify(websiteData));
-            }
-        );
-    } catch (err) {
-        console.log("err - >:", err);
-    }
+    await Promise.all([get(categoryPath), get(websitePathReq)]).then(
+      ([categoryData, websiteData]) => {
+        const cates = categoryData.map((it) => it.data);
+        localStorage.setItem('cate', JSON.stringify(cates));
+
+        const webs = websiteData.map((it) => it.data);
+        localStorage.setItem('site', JSON.stringify(webs));
+      },
+    );
+  } catch (err) {
+    console.log('err - >:', err);
+  }
 };
 
 const getFinalData = async () => {
-    if (!localStorage.getItem("cate") || !localStorage.getItem("site")) {
-        await fetchData();
-    }
+  if (!localStorage.getItem('cate') || !localStorage.getItem('site')) {
+    await fetchData();
+  }
 
-    return {
-        categoryData: JSON.parse(localStorage.getItem("cate") || "[]"),
-        websiteData: JSON.parse(localStorage.getItem("site") || "[]"),
-    };
+  return {
+    categoryData: JSON.parse(localStorage.getItem('cate') || '[]'),
+    websiteData: JSON.parse(localStorage.getItem('site') || '[]'),
+  };
 };
 
 export const getDataBase = async () => {
-    const { categoryData, websiteData } = await getFinalData();
+  const { categoryData, websiteData } = await getFinalData();
 
-    categoryData.forEach((item) => {
-        item.childrens = item.childrens.map((it) => websiteData.find((item) => it === item.id));
-    });
+  categoryData.forEach((item) => {
+    item.childrens = item.childrens.map((it) =>
+      websiteData.find((item) => it === item.id),
+    );
+  });
 
-    return categoryData;
+  return categoryData;
 };
 
 export const getDataByName = async (title) => {
-    const websiteData = JSON.parse(localStorage.getItem("site") || "[]");
+  const websiteData = JSON.parse(localStorage.getItem('site') || '[]');
 
-    const backData = websiteData;
+  const backData = websiteData;
 
-    if (!title) {
-        return backData;
-    }
+  if (!title) {
+    return backData;
+  }
 
-    return backData.filter((item) => {
-        return (
-            item.title?.toLowerCase().includes(title.toLowerCase()) ||
-            item.description?.toLowerCase().includes(title.toLowerCase()) ||
-            item.url?.toLowerCase().includes(title.toLowerCase())
-        );
-    });
+  return backData.filter((item) => {
+    return (
+      item.title?.toLowerCase().includes(title.toLowerCase()) ||
+      item.description?.toLowerCase().includes(title.toLowerCase()) ||
+      item.url?.toLowerCase().includes(title.toLowerCase())
+    );
+  });
 };
